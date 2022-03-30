efccd7fc145a88ddff8296c14828362cddccf76d
Raymond Chen <ahrchen@gmail.com>
2022-03-30 13:55:12 -0700

    Initial Commit

---
+++ AccessibilityPractice/AccessibilityPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityPracticeApp.swift
+//  AccessibilityPractice
+//
+//  Created by Raymond Chen on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilityPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AccessibilityPractice
+//
+//  Created by Raymond Chen on 3/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
